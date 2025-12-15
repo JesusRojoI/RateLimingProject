1d26e349c38fc0f98cab41517c67a2385674d5ab
JesusRojoI <jesro23us@gmail.com>
2025-12-15 14:47:53 -0600

    Auth routes & controllers

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { showLogin, showRegister, registerUser, loginUser, logoutUser, userPage } from "../controllers/authController.js";
+import { loginLimiter } from "../middleware/rateLimiter.js";
+
+const router = express.Router();
+
+router.get("/login", showLogin);
+router.post("/login", loginLimiter, loginUser);
+router.get("/register", showRegister);
+router.post("/register", registerUser);
+router.get("/user", userPage);
+router.get("/logout", logoutUser);
+
+export default router;
